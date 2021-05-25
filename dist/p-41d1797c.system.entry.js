5ee1f459f0901ba278b367e72911ba2ac6152d63
UiWebKit <uniwebkit@gmail.com>
2021-05-25 20:07:28 +0300

    fix: action-copy-wrap -> action-copy (0.1.0 -> 0.1.1)

--- dist/p-41d1797c.system.entry.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-393084f3.system.js","./p-a821f331.system.js","./p-0a0cddb8.system.js"],(function(t){"use strict";return{setters:[function(){},function(){},function(s){t("uni_action_copy_wrap",s.U)}],execute:function(){}}}));
--- dist/p-be15ffef.entry.js
+++
@@ -1,1 +0,0 @@
-import"./p-1e4a48b9.js";import"./p-78249524.js";export{U as uni_action_copy_wrap}from"./p-26d0c10b.js";
